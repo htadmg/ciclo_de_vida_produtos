355be501eb2079aa0947a3150cf26d080bfec49d
htadmg <agatadominguestec@hotmail.com>
2024-11-07 16:27:14 -0300

    criando lista com os produtos e navegação com as com os produtos

--- app.py
+++ app.py
@@ -7,7 +7,18 @@ with open('embalagens.json', 'r', encoding='utf-8') as f:
 
 @app.route('/')
 def index():
-    return render_template("embalagem.html", embalagens=embalagens)
+    materiais_unicos = set()
+    for embalagem in embalagens:
+        for material in embalagem['materiais']:
+            materiais = [m.strip() for m in material.split(',')]
+            for m in materiais:
+                materiais_unicos.add(m)
+    
+    # Convertendo o conjunto de volta para uma lista para passar para o template
+    materiais_unicos = list(materiais_unicos)
+    
+    return render_template('embalagem.html', embalagens=embalagens, materiais_unicos=materiais_unicos)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
